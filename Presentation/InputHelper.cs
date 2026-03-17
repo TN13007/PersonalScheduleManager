4f94d62f562421005e0ba1c3af6a7d87e65f07bf
abcd1234 <thuannguyen.31241023190@st.ueh.edu.vn>
2026-03-17 09:50:23 +0700

    Implement InputHelper class with method stubs

--- Presentation/InputHelper.cs
+++ Presentation/InputHelper.cs
@@ -1,1 +1,47 @@
+public class InputHelper
+{
+    public string ReadString(string prompt)
+    {
+        throw new NotImplementedException();
+    }
 
+    public int ReadInt(string prompt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DateTime ReadDateTime(string prompt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Priority ReadPriority(string prompt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Status ReadStatus(string prompt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TimeRange ReadTimeRange(string prompt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DisplayMessage(string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DisplayError(string error)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ClearScreen()
+    {
+        throw new NotImplementedException();
+    }
+}
